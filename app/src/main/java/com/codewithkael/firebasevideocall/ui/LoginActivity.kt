ec9d8ece3d53867fd234a5d60d0e896dc159dcb9
Lương Thuận Chung <49476209+thuanchung85@users.noreply.github.com>
2024-05-04 11:05:47 +0700

    save

--- app/src/main/java/com/codewithkael/firebasevideocall/ui/LoginActivity.kt
+++ app/src/main/java/com/codewithkael/firebasevideocall/ui/LoginActivity.kt
@@ -9,37 +9,46 @@ import com.codewithkael.firebasevideocall.repository.MainRepository
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
 
-
+//@AndroidEntryPoint is an annotation provided by the Hilt library for enabling dependency injection in Android components. It simplifies the process of injecting dependencies into Android apps and reduces the amount of boilerplate code required for managing dependencies.
 @AndroidEntryPoint
 class LoginActivity : AppCompatActivity() {
 
+    //ta dùng kỷ thuật view binding nên không cần dùng view findbyid nữa.
+    // import class tạm ActivityLoginBinding là truy cập được activity_login.xml
     private lateinit var  views:ActivityLoginBinding
+
     @Inject lateinit var mainRepository: MainRepository
 
+    //===on Create activity===//
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         views = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(views.root)
-        init()
-    }
 
-
-    private fun init(){
+        //===code cho UI login activity===//
         views.apply {
-            btn.setOnClickListener {
-                mainRepository.login(
-                    usernameEt.text.toString(),passwordEt.text.toString()
-                ){ isDone, reason ->
-                    if (!isDone){
-                        Toast.makeText(this@LoginActivity, reason, Toast.LENGTH_SHORT).show()
-                    }else{
+            btnSignIn.setOnClickListener {
+                //khi SignIn button click thi gọi mainRepository chạy task login
+                mainRepository.login(usernameEt.text.toString(),passwordEt.text.toString())
+                { isDone, reason ->
+                    //nếu login success
+                    if (isDone == true){
                         //start moving to our main activity
                         startActivity(Intent(this@LoginActivity, MainActivity::class.java).apply {
                             putExtra("username",usernameEt.text.toString())
                         })
+
+                    }
+                    //nếu login fail
+                    else{
+                        Toast.makeText(this@LoginActivity, reason, Toast.LENGTH_SHORT).show()
                     }
                 }
             }
         }
-    }
-}
+
+    }//end onCreate
+
+
+
+}//end class
--- app/src/main/java/com/codewithkael/firebasevideocall/utils/MyApplication.kt
+++ app/src/main/java/com/codewithkael/firebasevideocall/utils/MyApplication.kt
@@ -3,5 +3,9 @@ package com.codewithkael.firebasevideocall.utils
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
 
+//========ĐÂY LÀ CỔNG CHÍNH CỦA APP "MAIN ACTIVITY"====///
+//@HiltAndroidApp triggers Hilt's code generation,
+// including a base class for your application that can use dependency injection.
+//kích hoạt khả năng dependency injection cho toàn App bằng cách quy định đây là app thuộc dạng Hilt
 @HiltAndroidApp
 class MyApplication : Application()
